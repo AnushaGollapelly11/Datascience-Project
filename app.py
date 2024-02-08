d1f37afc1e4abb0c7e81f9ff6fa9c994fa91726c
AnushaGollapelly11 <gollapellyanusha12@gmail.com>
2024-02-08 19:54:04 +0530

    Commented out pd and plt

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ print (' ~ ~ ~ ~ ~ ~ ~ ~ OM SHRI GANESHAAYA NAMAHA ~ ~ ~ ~ ~ ~ ~ ~ ')
 
 import streamlit as st
 import joblib
-import pandas as pd
-import matplotlib.pyplot as plt
+#import pandas as pd
+#import matplotlib.pyplot as plt
 
 # Load the trained model
 model = joblib.load('model.pkl')
